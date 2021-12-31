c15760fc027e00d74216a29bf70fcf85a7cc5131
Nirob <82889679+nirobabhee@users.noreply.github.com>
2021-12-31 18:03:38 +0600

    update

--- laravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ laravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,8 +14,10 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
             $table->string('name');
+            $table->unsignedBigInteger('referrer_id');
+            $table->foreign('referrer_id')->references('id')->on('users');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
